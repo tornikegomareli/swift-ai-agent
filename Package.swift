21029b5fd5b6426c7494469d699f545d2e4fd24e
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-04-23 01:25:59 +0400

    package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "swift-ai-agent",
+  platforms: [
+    .macOS(
+      .v15
+    )
+  ],
+  targets: [
+    .executableTarget(
+      name: "swift-ai-agent"
+    )
+  ]
+)
